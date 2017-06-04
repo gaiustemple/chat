a50082ef4847354178846d3cb89d26c0df04dc67
gaiustemple <gaiustemple@gmail.com>
2017-06-04 20:09:10 +0100

    bug fix

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ MongoClient.connect(url, function(err, db){
         socket.on('reqCollection', function(data){
             console.log(data);
             /*var theCollection = "db.collection('" + data + "')";*/
-            db.collection('old3').find().toArray().then(function(docs){
+            db.collection('old2').find().toArray().then(function(docs){
                 io.emit('sendCollection', docs);
                 console.log("sent");
             });
